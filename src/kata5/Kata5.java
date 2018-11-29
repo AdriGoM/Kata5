ba91da35aded6607c5d7990946ab954db5926ace
AdriGoM <adriangodoymartinez@icloud.com>
2018-11-29 10:10:09 +0000

    Kata5 V3 adding registers to the database from the txt file

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -3,27 +3,44 @@ package kata5;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class Kata5 {
 
+    private static Connection c;
+
+    private static void connect() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+
+        c = DriverManager.getConnection("jdbc:sqlite:KATA5.db");
+        c.setAutoCommit(false);
+    }
+
+    private static void disconnect() throws SQLException {
+        c.close();
+    }
+
+    private static void insertIntoEmail(String email) throws SQLException {
+        String statement = "INSERT INTO EMAIL(Mail) values ('" + email + "')";
+        Statement s = c.createStatement();
+        s.execute(statement);
+        c.commit();
+        s.close();
+    }
+
     public static void main(String[] args) {
-
         try {
-            Class.forName("org.sqlite.JDBC");
+            connect();
 
-            Connection conexion;
-            conexion = DriverManager.getConnection("jdbc:sqlite:KATA5.db");
-            conexion.setAutoCommit(false);
+            List<String> emails = MailListReader.getInstance().read("/Users/adriangodoymartinez/IdeaProjects/Kata5/mail.txt");
 
-            Statement enunciado;
-            enunciado = conexion.createStatement();
+            for (String email : emails) {
+                insertIntoEmail(email);
+            }
 
-            enunciado.execute("CREATE TABLE 'EMAIL'('Id' INTEGER PRIMARY KEY AUTOINCREMENT, 'Mail' TEXT NOT NULL);");
-            conexion.commit();
-
-            enunciado.close();
-            conexion.close();
+            disconnect();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
